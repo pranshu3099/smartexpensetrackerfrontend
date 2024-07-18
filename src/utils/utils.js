d165d78e4ddb77b5f4c4a1204ceb6f4f4e1d00cd
pranshu <srivastavapranshu2@gmail.com>
2024-07-19 00:38:38 +0530

    retrieve data from localstorage

---
+++ src/utils/utils.js
@@ -0,0 +1,3 @@
+export const authmethod = localStorage.getItem("authmethod");
+export const userData = JSON.parse(localStorage.getItem("userdata"));
+export const bearer = localStorage.getItem("Bearer");
