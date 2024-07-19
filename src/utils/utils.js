eb3b66965de6ff0eac33f0a5f5d876fdc91b4529
pranshu <srivastavapranshu2@gmail.com>
2024-07-20 03:13:37 +0530

    utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,22 @@
 export const authmethod = localStorage.getItem("authmethod");
 export const userData = JSON.parse(localStorage.getItem("userdata"));
 export const bearer = localStorage.getItem("Bearer");
+export const getCategory = (target, lastTag, setLastTag) => {
+  if (lastTag !== "") {
+    lastTag.style.textDecoration = "none";
+  }
+  let finalTag = target;
+  if (target.tagName === "IMG") {
+    target = target.parentElement;
+    const pTag = target.querySelector("p");
+    finalTag = pTag;
+  }
+
+  setLastTag(finalTag);
+  finalTag.style.textDecoration = "underline";
+  finalTag.style.textDecorationColor = "gray";
+  finalTag.style.textUnderlineOffset = "6px";
+  finalTag.style.textDecorationThickness = "5px";
+
+  return finalTag.textContent;
+};
