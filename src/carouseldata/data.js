33c3fffa5eb339b25a498c5075fb5016eb73676b
pranshu <srivastavapranshu2@gmail.com>
2024-07-21 06:22:30 +0530

    added data for images

--- src/carouseldata/data.js
+++ src/carouseldata/data.js
@@ -12,3 +12,57 @@ export const data = [
       "Track and categorize your expenses easily to identify areas where you can save money.",
   },
 ];
+
+export const imageData = [
+  {
+    id: "1",
+    category: "Investment",
+    src: "/src/icons/investment-invest-svgrepo-com.svg",
+  },
+
+  {
+    id: "2",
+    category: "Taxes",
+    src: "/src/icons/taxes-bill-svgrepo-com.svg",
+  },
+  {
+    id: "3",
+    category: "Rent",
+    src: "/src/icons/rent-camping-svgrepo-com.svg",
+  },
+  {
+    id: "4",
+    category: "Gifts",
+    src: "/src/icons/gifts-svgrepo-com.svg",
+  },
+  {
+    id: "5",
+    category: "Education",
+    src: "/src/icons/education-laboratory-school-svgrepo-com.svg",
+  },
+  {
+    id: "6",
+    category: "Medical",
+    src: "/src/icons/medical-kit-svgrepo-com.svg",
+  },
+  {
+    id: "7",
+    category: "Travelling",
+    src: "/src/icons/travel-svgrepo-com.svg",
+  },
+  {
+    id: "8",
+    category: "Entertainment",
+    src: "/src/icons/entertainment-svgrepo-com.svg",
+  },
+  {
+    id: "9",
+    category: "Shopping",
+    src: "/src/icons/shopping-bag-svgrepo-com.svg",
+  },
+  {
+    id: "10",
+    category: "FoodandDining",
+    src: "/src/icons/food-and-drink-svgrepo-com.svg",
+  },
+];
