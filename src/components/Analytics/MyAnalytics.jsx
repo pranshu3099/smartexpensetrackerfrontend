6634fca867503675d8d9303fe906dbffd9d4ed6b
pranshu <srivastavapranshu2@gmail.com>
2024-07-30 02:57:27 +0530

    analytics to see monthly expense

--- src/components/Analytics/MyAnalytics.jsx
+++ src/components/Analytics/MyAnalytics.jsx
@@ -11,6 +11,7 @@ import {
   getMonthAndYear,
   months,
   GetMonthlyExpenseData,
+  category_name,
 } from "../../utils/utils";
 import "chart.js/auto";
 import "./Analytics.css";
@@ -19,6 +20,7 @@ const react_api_url = import.meta.env.VITE_REACT_APP_API_URL;
 const MyAnalytics = ({ onClose, setseeAnalytics }) => {
   const [analyticsData, setAnalyticsData] = useState([]);
   const [expenseData, setexpenseData] = useState([]);
+  const [categoryData, setcategoryData] = useState([]);
   useEffect(() => {
     const { month, year } = getMonthAndYear();
     const fetchData = async () => {
@@ -32,8 +34,10 @@ const MyAnalytics = ({ onClose, setseeAnalytics }) => {
   }, []);
   useEffect(() => {
     if (analyticsData.length > 0) {
-      const expense_data = GetMonthlyExpenseData(analyticsData);
-      setexpenseData(expense_data);
+      const { expense_result, category_result } =
+        GetMonthlyExpenseData(analyticsData);
+      setexpenseData(expense_result);
+      setcategoryData(category_result);
     }
   }, [analyticsData]);
   const handleclose = () => {
@@ -45,10 +49,11 @@ const MyAnalytics = ({ onClose, setseeAnalytics }) => {
       <ModalOverlay />
       <ModalContent width="1800px" maxWidth="1800px" height="800px">
         <ModalBody>
-          <h1>Your Monthy Analytics</h1>
+          <h1>Your Monthly Analytics</h1>
           <div className="chart-container">
-            <div className="Bar">
+            <div>
               <Bar
+                className="chart"
                 data={{
                   labels: months,
                   datasets: [
@@ -68,6 +73,32 @@ const MyAnalytics = ({ onClose, setseeAnalytics }) => {
                         "rgba(255,159,64,1)",
                         "rgba(54,162,235,1)",
                         "rgba(255,206,86,1)",
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </div>
+            <div>
+              <Pie
+                className="chart"
+                data={{
+                  labels: category_name,
+                  datasets: [
+                    {
+                      label: "Catergory",
+                      data: categoryData,
+                      backgroundColor: [
+                        "rgba(54,162,235,1)",
+                        "#ffa04090",
+                        "#56ffb9",
+                        "#dfff40",
+                        "#eb3636",
+                        "#5cff56",
+                        "#fffc40",
+                        "#eb3691",
+                        "#7256ff",
+                        "#ff8940",
                       ],
                     },
                   ],
